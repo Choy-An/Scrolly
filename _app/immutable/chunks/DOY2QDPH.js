5f20f294a4a85eeb681aef329638df685b82a064
Choy-An <aswanthchoyan@gmail.com>
2026-03-16 14:15:38 +0530

    deploy site

---
+++ _app/immutable/chunks/DOY2QDPH.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_feh1zw?.base??"/Scrolly",e=globalThis.__sveltekit_feh1zw?.assets??s??"";export{e as a,s as b};
---
+++ _app/immutable/entry/start.B_nc8MUX.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/DoL9GVV6.js";export{o as load_css,r as start};
